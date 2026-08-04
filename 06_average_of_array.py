78e8895c8cdf47372ad2c107eb2c919ae2e07623
Sohini Ball <ballsohini939@gmail.com>
2026-08-04 20:10:21 +0530

    Add array average program

---
+++ 06_average_of_array.py
@@ -0,0 +1,9 @@
+numbers = [10, 20, 30, 40, 50]
+total = 0
+count = 0
+for number in numbers:
+    total = total + number
+    count = count + 1
+average = total / count
+
+print("Average:", average)
